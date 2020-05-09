458168ac68e89234e7244ea959fac8c5ca9438a4
kapiekarz <piekarz.karolina@gmail.com>
2020-05-09 16:01:40 +0200

    Add localization

--- app/src/main/java/com/example/medapp/MainActivity.kt
+++ app/src/main/java/com/example/medapp/MainActivity.kt
@@ -29,6 +29,32 @@ class MainActivity : AppCompatActivity() {
         val tabs: TabLayout = findViewById(R.id.tabs)
         tabs.setupWithViewPager(viewPager)
 
+
+        locationManager = getSystemService(LOCATION_SERVICE) as LocationManager?
+
+        try {
+            // Request location updates
+            locationManager?.requestLocationUpdates(
+                LocationManager.NETWORK_PROVIDER,
+                0L,
+                0f,
+                locationListener
+            )
+        } catch (ex: SecurityException) {
+            Log.d("myTag", "Security Exception, no location available")
+        }
+
     }
 
+    //define the listener
+    private val locationListener: LocationListener = object : LocationListener {
+        override fun onLocationChanged(location: Location) {
+            thetext.text = ("" + location.longitude + ":" + location.latitude)
+
+        }
+
+        override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {}
+        override fun onProviderEnabled(provider: String) {}
+        override fun onProviderDisabled(provider: String) {}
+    }
 }
