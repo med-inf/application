e0cfc43f062c31329181c781de8f325cade61cec
kapiekarz <piekarz.karolina@gmail.com>
2020-05-09 16:01:03 +0200

    Initial commit

---
+++ app/src/main/java/com/example/medapp/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.medapp
+
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager.widget.ViewPager
+import com.androidnetworking.AndroidNetworking
+import com.androidnetworking.common.Priority
+import com.androidnetworking.error.ANError
+import com.androidnetworking.interfaces.JSONObjectRequestListener
+import com.example.medapp.ui.main.SectionsPagerAdapter
+import com.google.android.material.tabs.TabLayout
+import kotlinx.android.synthetic.main.fragment_main.*
+import org.json.JSONObject
+
+
+class MainActivity : AppCompatActivity() {
+    private var locationManager: LocationManager? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
+        val viewPager: ViewPager = findViewById(R.id.view_pager)
+        viewPager.adapter = sectionsPagerAdapter
+        val tabs: TabLayout = findViewById(R.id.tabs)
+        tabs.setupWithViewPager(viewPager)
+
+    }
+
+}
