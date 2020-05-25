370b0252714f3df62fa5a60776f2d3408431d081
kapiekarz <piekarz.karolina@gmail.com>
2020-05-25 10:44:30 +0200

    add listview

---
+++ medapp/src/main/java/com/example/medapp/Database.kt
@@ -0,0 +1,50 @@
+package com.example.medapp
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Entity
+data class UserLocation(
+    @PrimaryKey val uid: Int,
+    @ColumnInfo(name = "square_id") val squareId: String?,
+    @ColumnInfo(name = "timestamp") val timestamp: String?
+)
+
+@Dao
+interface LocationDao {
+    @Query("SELECT * FROM UserLocation")
+    fun getAll(): List<UserLocation>
+
+    @Insert
+    fun insertAll(vararg locations: UserLocation)
+
+    @Delete
+    fun delete(location: UserLocation)
+}
+
+@Database(entities = arrayOf(UserLocation::class), version = 1)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun locationDao(): LocationDao
+}
+
+
+class WordViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val repository: WordRepository
+    val allLocations: LiveData<List<UserLocation>>
+
+    init {
+        val wordsDao = WordRoomDatabase.getDatabase(application, viewModelScope).wordDao()
+        repository = WordRepository(wordsDao)
+        allWords = repository.allWords
+    }
+
+    /**
+     * Launching a new coroutine to insert the data in a non-blocking way
+     */
+    fun insert(word: Word) = viewModelScope.launch(Dispatchers.IO) {
+        repository.insert(word)
+    }
+}
--- medapp/src/main/java/com/example/medapp/MainActivity.kt
+++ medapp/src/main/java/com/example/medapp/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.medapp
 
 import android.Manifest
+import android.content.Context
+import android.content.SharedPreferences
 import android.content.pm.PackageManager
 import android.location.Location
 import android.location.LocationListener
@@ -12,19 +14,45 @@ import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.room.Room
 import com.androidnetworking.AndroidNetworking
 import com.androidnetworking.common.Priority
 import com.androidnetworking.error.ANError
 import com.androidnetworking.interfaces.JSONObjectRequestListener
 import org.json.JSONObject
+import java.lang.Math.abs
+import java.util.*
+import kotlin.collections.ArrayList
 
 
 class MainActivity : AppCompatActivity() {
     private var locationManager: LocationManager? = null
     private var locations = ArrayList<String>()
     private var adapter: MyRecyclerViewAdapter? = null
+    private lateinit var wordViewModel: WordViewModel
 
-    private var locationTimeBetweenMs : Long = 5000
+    private var locationTimeBetweenMs: Long = 5000
+    private var db: AppDatabase? = null
+
+    private var uniqueID: String? = null
+    private val PREF_UNIQUE_ID = "PREF_UNIQUE_ID"
+
+    @Synchronized
+    fun id(context: Context): String? {
+        if (uniqueID == null) {
+            val sharedPrefs: SharedPreferences = context.getSharedPreferences(
+                PREF_UNIQUE_ID, Context.MODE_PRIVATE
+            )
+            uniqueID = sharedPrefs.getString(PREF_UNIQUE_ID, null)
+            if (uniqueID == null) {
+                uniqueID = UUID.randomUUID().toString()
+                val editor: SharedPreferences.Editor = sharedPrefs.edit()
+                editor.putString(PREF_UNIQUE_ID, uniqueID)
+                editor.commit()
+            }
+        }
+        return uniqueID
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,13 +60,20 @@ class MainActivity : AppCompatActivity() {
 
         AndroidNetworking.initialize(getApplicationContext());
 
+        id(this)
+
+        db = Room.databaseBuilder(
+            applicationContext,
+            AppDatabase::class.java, "location-database"
+        ).build()
+
         // check location permissions
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(
-                        this,
-                        Manifest.permission.ACCESS_FINE_LOCATION
-                )
-                != PackageManager.PERMISSION_GRANTED
+            != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            )
+            != PackageManager.PERMISSION_GRANTED
         ) {
             ActivityCompat.requestPermissions(
                 this,
@@ -53,10 +88,10 @@ class MainActivity : AppCompatActivity() {
         locationManager = getSystemService(LOCATION_SERVICE) as LocationManager?
         try {
             locationManager?.requestLocationUpdates(
-                    LocationManager.NETWORK_PROVIDER,
-                    locationTimeBetweenMs,
-                    0f,
-                    locationListener
+                LocationManager.NETWORK_PROVIDER,
+                locationTimeBetweenMs,
+                0f,
+                locationListener
             )
         } catch (ex: SecurityException) {
             Log.d("myTag", "Security Exception, no location available")
@@ -71,45 +106,70 @@ class MainActivity : AppCompatActivity() {
     }
 
     private val locationListener: LocationListener = object : LocationListener {
-        override fun onLocationChanged(location: Location) {
-            locations.add("" + location.longitude + ":" + location.latitude)
-            adapter?.notifyDataSetChanged()
+        var prevSquareId: String = "c281af7f-830d-4c8b-8894-ba36d08d1aa7"
+        var center: Location = Location("")
+        var edgeLen: Double = 0.0
 
-            sendRequestToServer()
+        override fun onLocationChanged(location: Location) {
+            val isLocationChanged = checkIfLocationChanged(location);
+            if (isLocationChanged) {
+                val currentTime = Calendar.getInstance().time
+
+                sendRequestToServer(location, currentTime)
+            }
         }
 
-        fun sendRequestToServer(){
-                        val jsonString = """
+        fun checkIfLocationChanged(location: Location): Boolean {
+            if (kotlin.math.abs(location.latitude - center.latitude) < edgeLen / 2 && kotlin.math.abs(
+                    location.longitude - center.longitude
+                ) < edgeLen / 2
+            ) return false
+            return true
+        }
+
+        fun sendRequestToServer(location: Location, time: Date) {
+            val deviceId = uniqueID
+            println("id: $deviceId time: $time")
+            val jsonString = """
                 {
-                    "userId": "c281af7f-830d-4c8b-8894-ba36d08d1aa7",
-                    "time":"2020-05-06T19:03:34",
+                    "userId": "$deviceId",
+                    "time":"$time",
                     "position": {
-                   	 "lat": 10.0,
-                   	 "lon": 15.0
+                   	 "lat": ${location.latitude},
+                   	 "lon": ${location.longitude}
                     },
-                    "prevSquareId": "c281af7f-830d-4c8b-8894-ba36d08d1aa7"
+                    "prevSquareId": "$prevSquareId"
                 }
             """.trimIndent()
             val jsonObj = JSONObject(jsonString)
             AndroidNetworking.post("http://192.168.0.106:8080/getSquare")
-                    .addJSONObjectBody(jsonObj)
-                    .setTag(this)
-                    .setPriority(Priority.MEDIUM)
-                    .build()
-                    .getAsJSONObject(object : JSONObjectRequestListener {
-                        override fun onResponse(response: JSONObject) {
-                            System.out.println(response)
-                            val squareId: String =
-                                    response.getString("id")
-                        }
+                .addJSONObjectBody(jsonObj)
+                .setTag(this)
+                .setPriority(Priority.MEDIUM)
+                .build()
+                .getAsJSONObject(object : JSONObjectRequestListener {
+                    override fun onResponse(response: JSONObject) {
+                        println(response)
+                        prevSquareId =
+                            response.getString("id")
+                        center.latitude = response.getDouble("center.lat")
+                        center.longitude = response.getDouble("center.lon")
+                        edgeLen = response.getDouble("edgeLen")
+                        val time = response.getDouble("time")
+                        saveToDatabase(prevSquareId, time);
+                        locations.add(prevSquareId)
+                        adapter?.notifyDataSetChanged()
+                    }
 
-                        override fun onError(error: ANError) {
-                            System.out.println(error)
-                        }
-                    })
+                    override fun onError(error: ANError) {
+                        println(error)
+                    }
+                })
         }
+
         override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {}
         override fun onProviderEnabled(provider: String) {}
         override fun onProviderDisabled(provider: String) {}
     }
+
 }
