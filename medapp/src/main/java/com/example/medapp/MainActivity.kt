ebfe9050ff8eb016660735b62ce1323a695c8399
kapiekarz <piekarz.karolina@gmail.com>
2020-06-01 15:50:08 +0200

    bind service, but doesnt work

--- medapp/src/main/java/com/example/medapp/MainActivity.kt
+++ medapp/src/main/java/com/example/medapp/MainActivity.kt
@@ -2,68 +2,45 @@ package com.example.medapp
 
 import android.Manifest
 import android.annotation.SuppressLint
+import android.content.ComponentName
 import android.content.Context
-import android.content.SharedPreferences
+import android.content.Intent
+import android.content.ServiceConnection
 import android.content.pm.ActivityInfo
 import android.content.pm.PackageManager
-import android.location.Location
-import android.location.LocationListener
-import android.location.LocationManager
 import android.os.Bundle
-import android.util.Log
+import android.os.IBinder
+import android.telecom.ConnectionService
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.androidnetworking.AndroidNetworking
-import com.androidnetworking.common.Priority
-import com.androidnetworking.error.ANError
-import com.androidnetworking.interfaces.JSONObjectRequestListener
-import org.json.JSONObject
-import java.time.Instant
-import java.util.*
-import kotlin.collections.ArrayList
+import java.sql.Connection
 
 
 class MainActivity : AppCompatActivity() {
-    private var locationManager: LocationManager? = null
-    private var locations = ArrayList<String>()
-    private var adapter: MyRecyclerViewAdapter? = null
+    private lateinit var mService: MyService
+    private var mBound: Boolean = false
 
-    private var locationTimeBetweenMs: Long = 5000
-    private var locationPoint: Location? = null
+    /** Defines callbacks for service binding, passed to bindService()  */
+    private val connection = object : ServiceConnection {
 
-    private var uniqueID: String? = null
-    private val PREF_UNIQUE_ID = "PREF_UNIQUE_ID"
-
-    @Synchronized
-    fun id(context: Context): String? {
-        if (uniqueID == null) {
-            val sharedPrefs: SharedPreferences = context.getSharedPreferences(
-                PREF_UNIQUE_ID, Context.MODE_PRIVATE
-            )
-            uniqueID = sharedPrefs.getString(PREF_UNIQUE_ID, null)
-            if (uniqueID == null) {
-                uniqueID = UUID.randomUUID().toString()
-                val editor: SharedPreferences.Editor = sharedPrefs.edit()
-                editor.putString(PREF_UNIQUE_ID, uniqueID)
-                editor.commit()
-            }
+        override fun onServiceConnected(className: ComponentName, service: IBinder) {
+            // We've bound to LocalService, cast the IBinder and get LocalService instance
+            val binder = service as MyService.LocalBinder
+            mService = binder.getService()
+            mBound = true
         }
-        return uniqueID
+
+        override fun onServiceDisconnected(arg0: ComponentName) {
+            mBound = false
+        }
     }
 
-    @SuppressLint("SourceLockedOrientationActivity")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-
-        AndroidNetworking.initialize(getApplicationContext());
-
-        id(this)
 
         // check location permissions
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
@@ -77,135 +54,32 @@ class MainActivity : AppCompatActivity() {
                 this,
                 arrayOf(
                     Manifest.permission.ACCESS_COARSE_LOCATION,
-                    Manifest.permission.ACCESS_FINE_LOCATION
-//                    Manifest.permission.ACCESS_BACKGROUND_LOCATION
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_BACKGROUND_LOCATION
                 ), 1
             )
         }
-
-        // request location updates
-        locationManager = getSystemService(LOCATION_SERVICE) as LocationManager?
-        try {
-            locationManager?.requestLocationUpdates(
-                LocationManager.NETWORK_PROVIDER,
-                locationTimeBetweenMs,
-                0f,
-                locationListener
-            )
-        } catch (ex: SecurityException) {
-            Log.d("myTag", "Security Exception, no location available")
-        }
-
-
-        // set up the RecyclerView
-        val recyclerView: RecyclerView = findViewById(R.id.locationsList)
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        adapter = MyRecyclerViewAdapter(this, locations)
-        recyclerView.adapter = adapter
     }
 
     override fun onDestroy() {
-        super.onDestroy()
-        val jsonString = """
-                {
-                    "userId": "$uniqueID",
-                    "time":"1",
-                    "squareCenter": {
-                   	 "lat": ${locationPoint?.latitude},
-                   	 "lon": ${locationPoint?.longitude}
-                    }
-                }
-            """.trimIndent()
+//        val intent = Intent(this, MyService::class.java)
+//        stopService(intent)
+        mService.leaveSquare()
 
-        val jsonObj = JSONObject(jsonString)
-        AndroidNetworking.put("http://192.168.0.106:8080/leaveSquare")
-            .addJSONObjectBody(jsonObj)
-            .setTag(this)
-            .setPriority(Priority.MEDIUM)
-            .build();
+        super.onDestroy()
     }
 
-    private val locationListener: LocationListener = object : LocationListener {
-        var center: Location? = null
-        var edgeLen: Double = 0.0
-
-        override fun onLocationChanged(location: Location) {
-            if (center == null) {
-                sendRequestToServer(location)
-            } else {
-                val isLocationChanged = checkIfLocationChanged(location);
-                if (isLocationChanged) {
-                    sendRequestToServer(location)
-                }
-            }
+    override fun onStart() {
+        super.onStart()
+        // Bind to LocalService
+        Intent(this, MyService::class.java).also { intent ->
+            bindService(intent, connection, Context.BIND_AUTO_CREATE)
         }
+    }
 
-        fun checkIfLocationChanged(location: Location): Boolean {
-            if (kotlin.math.abs(location.latitude - center?.latitude!!) < edgeLen / 2 && kotlin.math.abs(
-                    location.longitude - center?.longitude!!
-                ) < edgeLen / 2
-            ) return false
-            return true
-        }
-
-        fun sendRequestToServer(location: Location) {
-            val deviceId = uniqueID
-
-            var jsonString = ""
-            if (center == null) {
-                jsonString = """
-                {
-                    "userId": "$deviceId",
-                    "time":${location.time},
-                    "position": {
-                   	 "lat": ${location.latitude},
-                   	 "lon": ${location.longitude}
-                    },
-                    "prevSquareCenter": null
-                }
-            """.trimIndent()
-            } else {
-                jsonString = """
-                {
-                    "userId": "$deviceId",
-                    "time":${location.time},
-                    "position": {
-                   	 "lat": ${location.latitude},
-                   	 "lon": ${location.longitude}
-                    },
-                    "prevSquareCenter": {
-                   	 "lat": ${center?.latitude},
-                   	 "lon": ${center?.longitude}
-                    }
-                }
-            """.trimIndent()
-            }
-            val jsonObj = JSONObject(jsonString)
-            AndroidNetworking.post("http://192.168.0.106:8080/getSquare")
-                .addJSONObjectBody(jsonObj)
-                .setTag(this)
-                .setPriority(Priority.MEDIUM)
-                .build()
-                .getAsJSONObject(object : JSONObjectRequestListener {
-                    override fun onResponse(response: JSONObject) {
-                        println("${location.latitude};${location.longitude} -- center: ${center?.latitude};${center?.longitude} -- edgeLen: $edgeLen")
-                        center = Location("")
-                        center?.latitude = response.getJSONObject("center").getDouble("lat")
-                        center?.longitude = response.getJSONObject("center").getDouble("lon")
-                        locationPoint = center
-                        edgeLen = response.getDouble("edgeLen")
-                        locations.add("${location.latitude};${location.longitude}")
-                        adapter?.notifyDataSetChanged()
-                    }
-
-                    override fun onError(error: ANError) {
-                        println(error)
-                    }
-                })
-        }
-
-        override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {}
-        override fun onProviderEnabled(provider: String) {}
-        override fun onProviderDisabled(provider: String) {}
+    override fun onStop() {
+        super.onStop()
+        unbindService(connection)
+        mBound = false
     }
 }
---
+++ medapp/src/main/java/com/example/medapp/MyService.kt
@@ -0,0 +1,286 @@
+package com.example.medapp
+
+import android.app.Service
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
+import android.location.Location
+import android.location.LocationManager
+import android.os.Binder
+import android.os.Bundle
+import android.os.IBinder
+import android.util.Log
+import com.androidnetworking.AndroidNetworking
+import com.androidnetworking.common.Priority
+import com.androidnetworking.error.ANError
+import com.androidnetworking.interfaces.JSONObjectRequestListener
+import org.json.JSONObject
+import java.util.*
+
+class MyService : Service() {
+    // Binder given to clients
+    private val binder = LocalBinder()
+
+    private var mLocationManager: LocationManager? = null
+
+    private var uniqueID: String? = null
+    private val PREF_UNIQUE_ID = "PREF_UNIQUE_ID"
+
+    private var locationPoint: Location? = null
+
+    private inner class LocationListener(provider: String) : android.location.LocationListener {
+        var mLastLocation: Location
+
+        var center: Location? = null
+        var edgeLen: Double = 0.0
+
+        override fun onLocationChanged(location: Location) {
+            Log.e(TAG, "onLocationChanged: $location")
+            mLastLocation.set(location)
+            if (center == null) {
+                sendRequestToServer(location)
+            } else {
+                val isLocationChanged = checkIfLocationChanged(location);
+                if (isLocationChanged) {
+                    sendRequestToServer(location)
+                }
+            }
+        }
+
+        override fun onProviderDisabled(provider: String) {
+            Log.e(TAG, "onProviderDisabled: $provider")
+        }
+
+        override fun onProviderEnabled(provider: String) {
+            Log.e(TAG, "onProviderEnabled: $provider")
+        }
+
+        override fun onStatusChanged(
+            provider: String,
+            status: Int,
+            extras: Bundle
+        ) {
+            Log.e(TAG, "onStatusChanged: $provider")
+        }
+
+        init {
+            Log.e(TAG, "LocationListener $provider")
+            mLastLocation = Location(provider)
+        }
+
+        fun checkIfLocationChanged(location: Location): Boolean {
+            if (kotlin.math.abs(
+                    location.latitude - (center?.latitude ?: 0.0)
+                ) < edgeLen / 2 && kotlin.math.abs(
+                    location.longitude - (center?.longitude ?: 0.0)
+                ) < edgeLen / 2
+            ) return false
+            return true
+        }
+
+        fun sendRequestToServer(location: Location) {
+            val deviceId = uniqueID
+            println(location)
+            var jsonString = ""
+            if (center == null) {
+                jsonString = """
+                {
+                    "userId": "$deviceId",
+                    "time":${location.time},
+                    "position": {
+                   	 "lat": ${location.latitude},
+                   	 "lon": ${location.longitude}
+                    },
+                    "prevSquareCenter": null
+                }
+            """.trimIndent()
+            } else {
+                jsonString = """
+                {
+                    "userId": "$deviceId",
+                    "time":${location.time},
+                    "position": {
+                   	 "lat": ${location.latitude},
+                   	 "lon": ${location.longitude}
+                    },
+                    "prevSquareCenter": {
+                   	 "lat": ${center?.latitude},
+                   	 "lon": ${center?.longitude}
+                    }
+                }
+            """.trimIndent()
+            }
+            val jsonObj = JSONObject(jsonString)
+            AndroidNetworking.post(GET_SQUARE_ENDPOINT)
+                .addJSONObjectBody(jsonObj)
+                .setTag(this)
+                .setPriority(Priority.MEDIUM)
+                .build()
+                .getAsJSONObject(object : JSONObjectRequestListener {
+                    override fun onResponse(response: JSONObject) {
+                        println("${location.latitude};${location.longitude} -- center: ${center?.latitude};${center?.longitude} -- edgeLen: $edgeLen")
+                        center = Location("")
+                        center?.latitude = response.getJSONObject("center").getDouble("lat")
+                        center?.longitude = response.getJSONObject("center").getDouble("lon")
+                        locationPoint = center
+                        edgeLen = response.getDouble("edgeLen")
+                    }
+
+                    override fun onError(error: ANError) {
+                        println(error)
+                    }
+                })
+
+
+        }
+    }
+
+    private var mLocationListeners = arrayOf(
+        LocationListener(LocationManager.GPS_PROVIDER),
+        LocationListener(LocationManager.NETWORK_PROVIDER)
+    )
+
+    override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
+        Log.e(TAG, "onStartCommand")
+        super.onStartCommand(intent, flags, startId)
+        return START_STICKY
+    }
+
+    override fun onCreate() {
+        Log.e(TAG, "onCreate")
+        id(this)
+        AndroidNetworking.initialize(getApplicationContext());
+        initializeLocationManager()
+        try {
+            mLocationManager?.requestLocationUpdates(
+                LocationManager.NETWORK_PROVIDER,
+                LOCATION_INTERVAL.toLong(),
+                LOCATION_DISTANCE,
+                mLocationListeners[1]
+            )
+        } catch (ex: SecurityException) {
+            Log.i(
+                TAG,
+                "fail to request location update, ignore",
+                ex
+            )
+        } catch (ex: IllegalArgumentException) {
+            Log.d(
+                TAG,
+                "network provider does not exist, " + ex.message
+            )
+        }
+        try {
+            mLocationManager?.requestLocationUpdates(
+                LocationManager.GPS_PROVIDER,
+                LOCATION_INTERVAL.toLong(),
+                LOCATION_DISTANCE,
+                mLocationListeners[0]
+            )
+        } catch (ex: SecurityException) {
+            Log.i(
+                TAG,
+                "fail to request location update, ignore",
+                ex
+            )
+        } catch (ex: IllegalArgumentException) {
+            Log.d(TAG, "gps provider does not exist " + ex.message)
+        }
+    }
+
+    fun leaveSquare(){
+        val jsonString = """
+                {
+                    "userId": "$uniqueID",
+                    "time":"1",
+                    "squareCenter": {
+                   	 "lat": ${locationPoint?.latitude},
+                   	 "lon": ${locationPoint?.longitude}
+                    }
+                }
+            """.trimIndent()
+
+        val jsonObj = JSONObject(jsonString)
+
+        AndroidNetworking.put(LEAVE_SQUARE_ENDPOINT)
+            .addJSONObjectBody(jsonObj)
+            .setTag(this)
+            .setPriority(Priority.MEDIUM)
+            .build().getAsJSONObject(object : JSONObjectRequestListener {
+                override fun onResponse(response: JSONObject) {
+                    this@MyService.stopSelf();
+                }
+                override fun onError(error: ANError) {
+                    println(error)
+                    this@MyService.stopSelf();
+                }
+            });
+
+    }
+
+    override fun onDestroy() {
+        Log.e(TAG, "onDestroy")
+
+        if (mLocationManager != null) {
+            for (i in mLocationListeners.indices) {
+                try {
+                    mLocationManager?.removeUpdates(mLocationListeners[i])
+                } catch (ex: Exception) {
+                    Log.i(
+                        TAG,
+                        "fail to remove location listners, ignore",
+                        ex
+                    )
+                }
+            }
+        }
+        super.onDestroy()
+    }
+
+    private fun initializeLocationManager() {
+        Log.e(TAG, "initializeLocationManager")
+        if (mLocationManager == null) {
+            mLocationManager =
+                applicationContext.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        }
+    }
+
+    companion object {
+        private const val TAG = "TESTGPS"
+        private const val LOCATION_INTERVAL = 0
+        private const val LOCATION_DISTANCE = 0f
+        private const val GET_SQUARE_ENDPOINT = "http://192.168.0.106:8080/getSquare"
+        private const val LEAVE_SQUARE_ENDPOINT = "http://192.168.0.106:8080/leaveSquare"
+    }
+
+
+    @Synchronized
+    fun id(context: Context): String? {
+        if (uniqueID == null) {
+            val sharedPrefs: SharedPreferences = context.getSharedPreferences(
+                PREF_UNIQUE_ID, Context.MODE_PRIVATE
+            )
+            uniqueID = sharedPrefs.getString(PREF_UNIQUE_ID, null)
+            if (uniqueID == null) {
+                uniqueID = UUID.randomUUID().toString()
+                val editor: SharedPreferences.Editor = sharedPrefs.edit()
+                editor.putString(PREF_UNIQUE_ID, uniqueID)
+                editor.commit()
+            }
+        }
+        return uniqueID
+    }
+
+    /**
+     * Class used for the client Binder.  Because we know this service always
+     * runs in the same process as its clients, we don't need to deal with IPC.
+     */
+    inner class LocalBinder : Binder() {
+        // Return this instance of LocalService so clients can call public methods
+        fun getService(): MyService = this@MyService
+    }
+
+    override fun onBind(intent: Intent): IBinder {
+        return binder
+    }
+}
