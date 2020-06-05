14b34deda447582c94fbcfe2fe88649e6dc452b5
kapiekarz <piekarz.karolina@gmail.com>
2020-06-05 17:35:10 +0200

    Add status

--- medapp/src/main/java/com/example/medapp/MainActivity.kt
+++ medapp/src/main/java/com/example/medapp/MainActivity.kt
@@ -1,26 +1,35 @@
 package com.example.medapp
 
 import android.Manifest
-import android.annotation.SuppressLint
-import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
-import android.content.ServiceConnection
-import android.content.pm.ActivityInfo
+import android.content.SharedPreferences
 import android.content.pm.PackageManager
-import android.os.*
-import android.telecom.ConnectionService
+import android.os.Bundle
+import android.os.Handler
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.androidnetworking.AndroidNetworking
-import java.sql.Connection
+import com.androidnetworking.common.Priority
+import com.androidnetworking.error.ANError
+import com.androidnetworking.interfaces.StringRequestListener
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mHandler: Handler
+
+    private var uniqueID: String? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        AndroidNetworking.initialize(getApplicationContext());
 
         // check location permissions
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
@@ -39,8 +48,90 @@ class MainActivity : AppCompatActivity() {
                 ), 1
             )
         }
+        id(this)
+        println(uniqueID)
 
         val intent = Intent(this, MyService::class.java)
         startService(intent)
+
+        mHandler = Handler()
+        startRepeatingTask()
+    }
+
+    private fun changeStatusToDanger(){
+        val img: ImageView = findViewById<ImageView>(R.id.status_image)
+        img.setImageResource(android.R.drawable.ic_delete)
+        val text: TextView = findViewById<TextView>(R.id.main_text)
+        text.text = "You may be infected. Please contact a doctor."
+    }
+
+    private fun changeStatusToGood(){
+        val img: ImageView = findViewById<ImageView>(R.id.status_image)
+        img.setImageResource(android.R.drawable.ic_input_add)
+        val text: TextView = findViewById<TextView>(R.id.main_text)
+        text.text = ""
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        stopRepeatingTask()
+    }
+
+    private var mStatusChecker: Runnable = object : Runnable {
+        override fun run() {
+            AndroidNetworking.get(CHECK_INFECTED_ENDPOINT)
+                .addQueryParameter("userId", uniqueID)
+                .setTag(this)
+                .setPriority(Priority.MEDIUM)
+                .build()
+                .getAsString(object : StringRequestListener {
+                    override fun onResponse(response: String?) {
+                        if (response.equals("true")){
+                            changeStatusToDanger()
+                        } else {
+                            changeStatusToGood()
+                        }
+                    }
+
+                    override fun onError(error: ANError) {
+                        println(error.errorBody)
+                    }
+                })
+
+            mHandler.postDelayed(this, CHECK_HEALTH_INTERVAL.toLong())
+        }
+    }
+
+    private fun startRepeatingTask() {
+        mStatusChecker.run()
+    }
+
+    private fun stopRepeatingTask() {
+        mHandler.removeCallbacks(mStatusChecker)
+    }
+
+    companion object {
+        private const val CHECK_INFECTED_ENDPOINT = "http://192.168.0.106:8080/infected"
+        private const val PREF_UNIQUE_ID = "PREF_UNIQUE_ID"
+        private const val CHECK_HEALTH_INTERVAL = 5000
+    }
+
+
+    @Synchronized
+    fun id(context: Context): String? {
+        if (uniqueID == null) {
+            val sharedPrefs: SharedPreferences = context.getSharedPreferences(
+                PREF_UNIQUE_ID, Context.MODE_PRIVATE
+            )
+            uniqueID = sharedPrefs.getString(PREF_UNIQUE_ID, null)
+            if (uniqueID == null) {
+                uniqueID = UUID.randomUUID().toString()
+                val editor: SharedPreferences.Editor = sharedPrefs.edit()
+                editor.putString(PREF_UNIQUE_ID, uniqueID)
+                editor.commit()
+            }
+        }
+        return uniqueID
     }
 }
